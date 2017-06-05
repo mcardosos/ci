c4e59702c8644a2c1c3a21d11c30b1e9856a4a16
mcardosos <mcardoso@microsoft.com>
2017-06-05 13:36:14 -0700

    Create folder

--- kube.sh
+++ kube.sh
@@ -1,5 +1,6 @@
 set -x
 sudo curl -O https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz
+mkdir go
 sudo tar -xvf go1.8.3.linux-amd64.tar.gz > /go
 export PATH=$PATH:/go/bin
 export GOPATH=`pwd`/goWorkspace
