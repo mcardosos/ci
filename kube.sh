b9f4127b7b3a3c1fe3dbcb6859cd622a18be3131
mcardosos <mcardoso@microsoft.com>
2017-06-05 13:43:08 -0700

    Install make

--- kube.sh
+++ kube.sh
@@ -1,4 +1,5 @@
 set -x
+sudo apt install make
 sudo curl -O https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz
 sudo tar -xvf go1.8.3.linux-amd64.tar.gz > /dev/null
 sudo mv go /usr/local/
@@ -6,5 +7,6 @@ export PATH=$PATH:/usr/local/go/bin
 export GOPATH=`pwd`/goWorkspace
 go get -d k8s.io/kubernetes
 cd $GOPATH/src/k8s.io/kubernetes
-CGO_ENABLED=0 go install -a -installsuffix cgo std
+CGO_ENABLED=0
+go install -a -installsuffix cgo std
 make
