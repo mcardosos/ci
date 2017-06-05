932cbd3d00327d0b73907f04c61be152594a44bf
mcardosos <mcardoso@microsoft.com>
2017-06-05 13:15:45 -0700

    Go lives in hime Rebuild stdlib

--- kube.sh
+++ kube.sh
@@ -1,8 +1,8 @@
 sudo curl -O https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz
-sudo tar -xvf go1.8.3.linux-amd64.tar.gz > /dev/null
-sudo mv go /usr/local/
-export PATH=$PATH:/usr/local/go/bin
+sudo tar -xvf go1.8.3.linux-amd64.tar.gz > /go
+export PATH=$PATH:/go/bin
 export GOPATH=`pwd`/goWorkspace
 go get -d k8s.io/kubernetes
 cd $GOPATH/src/k8s.io/kubernetes
+CGO_ENABLED=0 go install -a -installsuffix cgo std
 make
